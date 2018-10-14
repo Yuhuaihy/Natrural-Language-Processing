27d2aebea8564ca7e1dacc5c19435129172471c7
Yuhuaihy <yhuai@terpmail.umd.edu>
2018-10-13 22:31:15 -0400

    .

--- jupter-notebook/1009.py
+++ jupter-notebook/1009.py
@@ -6,18 +6,20 @@ print(infile)
 #     print(line)
 with open(infile,'r') as f:
     print(f)
-    #print(f.read())
+    s = f.read()
+    print(s)
+    print(type(s))
     #for line in f.read():  f.read()----string
-    for line in f:
-        print(line)
-        print('----')
+    # for line in f:
+    #     print(line)
+    #     print('----')
     
-a = input('input:')
-print(a)
-s = set([1,2,3])
-s1 = set([2,3])
-s2 = {1,2,3}
-d = set()
-d.add(s)
-d.add(s2)
-d.add(s1)
+# a = input('input:')
+# print(a)
+# s = set([1,2,3])
+# s1 = set([2,3])
+# s2 = {1,2,3}
+# d = set()
+# d.add(s)
+# d.add(s2)
+# d.add(s1)
